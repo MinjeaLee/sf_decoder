5255a7db98b5c373f6300801a92d6d346c2c4b30
LeeMinjea <gabe0529@naver.com>
2023-06-26 01:27:49 +0900

    Mod : qr 모달 형식으로 변경

---
+++ static/emperor.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+
+var modal = document.getElementById("imageModal");
+var btn = document.getElementById("popupButton");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    event.stopPropagation();
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
