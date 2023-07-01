4af31be83ae0a6a25527c1cada0b4d88f6809d0a
LeeMinjea <gabe0529@naver.com>
2023-07-01 18:43:46 +0900

    ADD : 여러 가지 기능 추가  1. 인코딩 서비스 무결성, 가용성 확인   2. 로그아웃 절차 확인Emperor 계정 확인   3. Emperor 계정 확인

--- ctf/SLA.py
+++ ctf/SLA.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import base64
+import time
 
 #todo
 #! TODO : 1. 로그인 회원가입 절차 확인
@@ -9,11 +10,13 @@ import base64
 #TODO : 2. 인코딩 서비스 무결성, 가용성 확인
 #TODO : 3. 로그아웃 절차 확인
 #TODO : 4. Emperor 계정 확인
-#TODO : 5. 
+#TODO : 5. Emperor page 접속 확인
+#TODO : 5-2. Emperor page에서 추가한 유저 정보 일치 여부 확인
+#TODO : 5-3. 추가한 유저, 작성 글 삭제 여부 확인
+#TODO : 5-4. flag qr 코드 확인
 
 
 def encode_base64(data):
-	# bytes = data.encode('ascii')
 	bytes = data.encode('utf-8')
 	encoded = base64.b64encode(bytes)
 	result = encoded.decode('ascii')
@@ -25,6 +28,13 @@ user = {
     'username' : "SLA" + str(random.randint(1, 100000)),
     'password' : "SLA" + str(random.randint(1, 100000))
 }
+
+flag = open("../flag.txt", "r").read()
+emperor = {
+	'username': "emperor",
+	'password': flag
+}
+print(emperor['password'])
 
 # 로그인 회원가입 절차 확인
 try:
@@ -48,12 +58,6 @@ if username_check.split(" ")[3] != user['username']:
 	exit()
 
 #TODO : 1-2. Emperor 회원 가입 불가 확인
-flag = open("../flag.txt", "r").read()
-emperor = {
-	'username' : "emperor",
-	'password' : flag
-}
-
 register = requests.post(check_url + "/register", data=emperor)
 soup = BeautifulSoup(register.text, 'html.parser')
 register_check = soup.find(
@@ -75,14 +79,74 @@ if mis_emperor_login != "alert(\"Login failed, check your username or password\"
 # login
 session = requests.Session()
 login = session.post(check_url + "/login", data=user)
+# print(login.text)
 
-for i in range(1):
+for i in range(3):
 	#random string 생성
 	random_string = str(random.randint(1, 10000000000))
+	result = encode_base64(random_string)
 	# request
-	target_url = "http://localhost:4242/service_post"
 	data = {'content': random_string}
 	cookies = session.cookies.get_dict()
-	response = requests.post(target_url, data=data, cookies=cookies)
+	# 시간 측정
+	start = time.time()
+	response = requests.post(check_url + "/service_post", data=data, cookies=cookies)
+	end = time.time()
+	elapse = end - start
 	data = response.text
-	print(data)
+	soup = BeautifulSoup(data, 'html.parser')
+	# media-body인 모든 요소를 가져옴
+	text = soup.find_all('div', {'class': 'media-body'})
+	# print(plain)
+	if i == 0:
+		plain_text = text[1].text.strip()
+		encode_text = text[2].text.strip()
+	else:
+		plain_text = text[2 * i].text.strip()
+		encode_text = text[2 * i + 1].text.strip()
+	if plain_text != random_string:
+		print("plain_text is not same random_string")
+		exit()
+	if encode_text != result:
+		print("encode_text is not same result")
+		exit()
+	if elapse > 0.5:
+		print("elapse time is too long")
+		exit()
+
+#TODO : 3. 로그아웃 절차 확인
+logout = session.get(check_url + "/logout")
+# 쿠키 확인
+cookies = session.cookies.get_dict()
+if cookies != {}:
+	print("logout failed")
+	exit()
+
+#TODO : 4. Emperor 계정 확인
+# login
+emperor["password"] = flag
+session = requests.Session()
+login = session.post(check_url + "/login", data=emperor)
+cookies = session.cookies.get_dict()
+if cookies == {}:
+	print("login failed")
+	exit()
+
+# emperor 페이지 확인
+connect_emperor = session.get(check_url + "/emperor")
+# print(connect_emperor.text)
+soup = BeautifulSoup(connect_emperor.text, 'html.parser')
+# td
+emperor_check = soup.find_all('td')
+id = emperor_check[0].text.strip()
+pw = emperor_check[1].text.strip()
+if id != user['username']:
+	print("emperor page incorrect runnig")
+	exit()
+if pw != user['password']:
+	print("emperor page incorrect runnig")
+	exit()
+
+
+# about 페이지 확인
+# response = session.get(check_url + "/about")
